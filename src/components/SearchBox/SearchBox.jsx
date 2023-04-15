a8623db4b585cdd229078bc584df8a4d5ec86e3a
Muffcata <marta.oched@gmail.com>
2023-04-15 22:51:43 +0200

    search movies - task done

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import style from 'components/SearchBox/SearchBox.module.css';
 
 export const SearchBox = ({ onSubmit }) => {
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="query" placeholder="Search here" />
-      <button type="submit">Search</button>
+    <form className={style.wrapper} onSubmit={onSubmit}>
+      <input
+        className={style.input}
+        type="text"
+        name="query"
+        placeholder="Search here"
+      />
+      <button className={style.icon} type="submit"></button>
     </form>
   );
 };
